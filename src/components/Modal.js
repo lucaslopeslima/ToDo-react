bcc2e5573da758d46bae4ded64942c95bb4aa025
Lucas L. Lima <lucaslima.webdev@gmail.com>
2022-04-11 16:11:37 -0300

    up

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,8 +1,17 @@
-import react from "react";
+import React, {useState} from "react";
 import Card from "./card";
 
     function Modal(props){
-        return(<div className="modal">
+        
+        function hideModal(e){
+            let target = e.target
+        if (target.id == "modal"){
+            props.onHideModal()
+        }
+        console.log(target)
+        }
+
+        return(<div id="modal" onClick={hideModal} className={props.show?"modal" : "modal hide"}>
             <Card className="cardModal">
                 {props.children}
 
