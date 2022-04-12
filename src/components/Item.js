92e7c531002906c856276b1dc35648969338cee3
Lucas L. Lima <lucaslima.webdev@gmail.com>
2022-04-12 00:21:08 -0300

    up

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,7 +3,9 @@ class Item{
     static lastId = 0
 
     constructor(text){
+        
         this.id = Item.lastId++
+        /* this.id = Date.now().toString('16') */
         this.text = text
         this.done = false
     }
