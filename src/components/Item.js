a7735ed0047403b975b268bb1070bdc9a3d75597
Lucas L. Lima <lucaslopes.developer@gmail.com>
2022-04-12 00:51:08 -0300

    up

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,8 +4,9 @@ class Item{
 
     constructor(text){
         
-        this.id = Item.lastId++
-        /* this.id = Date.now().toString('16') */
+        /* this.id = Item.lastId++ */
+        this.id = Date.now().toString('16')
+        /* this.id = Date.now().toString('16') + Math.random().toString('16').substring(2) */
         this.text = text
         this.done = false
     }
